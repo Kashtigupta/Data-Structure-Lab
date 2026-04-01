1f67455b6965829b974f20d43a0c355c5f702e67
Kashtigupta <kashti.gupta.batch2025@sitnagpur.siu.edu.in>
2026-04-01 09:51:10 +0530

    Update 3.1.1. Merge Sort.c

--- 3.1.1. Merge Sort.c
+++ 3.1.1. Merge Sort.c
@@ -1,73 +1,69 @@
 #include <stdio.h>
 
 void merge(int arr[], int left, int mid, int right) {
- int i, j, k;
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
+int h=left,i=left,j=mid+1;
+	int temp[15];
 
-    int L[n1], R[n2];
-
-    for (i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
-
-    i = 0;
-    j = 0;
-    k = left;
-
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
+	while(h<=mid && j<=right){
+		if(arr[h]<=arr[j]){
+			temp[i]=arr[h];
+			h++;
+		}else{
+			temp[i]=arr[j];
+			j++;
+		}
+		i++;
 	}
-
-
+	while(j<=right){
+		temp[i]=arr[j];
+		j++;
+		i++;
+	}
+	while(h<=mid){
+		temp[i]=arr[h];
+		h++;
+		i++;
+	}
+	
+	for(i=left;i<=right;i++){
+		arr[i]=temp[i];
+	}
 }
+
 void mergeSort(int arr[], int left, int right) {
-if (left < right) {
-        int mid = left + (right - left) / 2;
-
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
-        merge(arr, left, mid, right);
+	int mid;
+  if(left<right){
+	  mid=(left+right)/2;
+	  mergeSort(arr,left,mid);
+	  mergeSort(arr,mid+1,right);
+	  merge(arr,left,mid,right);
+  }
+  
+  
+   
 }
-}
+
 void printArray(int arr[], int size) {
-for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+int i;
+	for(i=0;i<size;i++){
+	printf("%d ", arr[i]);
+}
+	printf("\n");
 }
 
 int main() {
     int n;
+    
     printf("");
     scanf("%d", &n);
     
     int arr[n];
+    
     printf("");
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
+
     printf("");
     printArray(arr, n);
 
