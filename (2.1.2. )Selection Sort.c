fd5f44abab8e1bfb08245874cee2ec2cff93415d
Kashtigupta <kashti.gupta.batch2025@sitnagpur.siu.edu.in>
2026-04-01 09:49:14 +0530

    Update (2.1.2. )Selection Sort.c

--- (2.1.2. )Selection Sort.c
+++ (2.1.2. )Selection Sort.c
@@ -1,46 +1,35 @@
-#include <stdio.h>
+#include<stdio.h>
+void main() {
+	int a[20], i, n, j, large, index;
+	printf("Enter value of n : ");
+	scanf("%d", &n);
 
-void selectionSort(int arr[], int n) {
-for (int i = 0; i < n - 1; i++) {
-    int maxIndex = i;
+		printf("Enter element for a[%d] : ", i);
+		scanf("%d", &a[i]);
+	}
+	
+	printf("Before sorting the elements in the array are\n");
+	for(i=0;i<n;i++){
+		printf("Value of a[%d] = %d\n", i,a[i]);
+	}
+	
+	for(i=0;i<n-1;i++){
+		large = a[i];
+		index = i;
+		for(j=i+1;j<n;j++){
+			if(a[j]<large){
+			large = a[j];
+			index = j;
+		}
+		}
+			a[index]=a[i];
+			a[i]=large;
+	}
 
-    for (int j = i + 1; j < n; j++) {
-        if (arr[j] > arr[maxIndex]) {
-            maxIndex = j;
-        }
-    }
-
-    if (maxIndex != i) {
-        int temp = arr[i];
-        arr[i] = arr[maxIndex];
-        arr[maxIndex] = temp;
-    }
-}
-
-
-}
-
-int main() {
-    int n;
-
-    // Input number of elements in the array
-    scanf("%d", &n);
-
-    int arr[n];
-
-    // Input array elements
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    // Sort the array in descending order using selection sort
-    selectionSort(arr, n);
-
-    // Output the sorted array
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    return 0;
+	printf("After sorting the elements in the array are\n");
+	for(i=0;i<n;i++){
+		printf("Value of a[%d] = %d\n", i,a[i]);
+	}
+	
+	
 }
